88361b06c7d3ebb23637a01a4f34c7e69c259fbd
Susuper_Peng <86904920+SusuperPeng@users.noreply.github.com>
2025-06-13 14:40:04 +0800

    Little_Pass.h

--- Little_Pass.h
+++ Little_Pass.h
@@ -2,7 +2,9 @@
 #define __Little_Pass_H
  
 #include "stm32f10x.h"
- 
+
+extern char new_flag;
+
 float Filter_RC_Simple(float NEW_DATA,float OLD_DATA);
 float Filter_RC(float NEW_DATA,float OLD_DATA,char filter_flag);
 
